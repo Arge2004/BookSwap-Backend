2b55f9078177f983e2f7d45059b2f9677c6e31f3
Argenis Medina <argenis.medina@udea.edu.co>
2024-11-26 16:54:25 -0500

    cambios para el deploy

--- src/main/java/BookSwap/BookSwapApplication.java
+++ src/main/java/BookSwap/BookSwapApplication.java
@@ -10,7 +10,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class BookSwapApplication {
 
 	public static void main(String[] args) {
+		String port = System.getenv("PORT");
+		if(port != null) {
+			System.setProperty("server.port", port);
+		}
 		SpringApplication.run(BookSwapApplication.class, args);
 	}
-
 }
