e54796d9970120ff229966ea35b3b8bc36021631
Argenis Medina <argenis.medina@udea.edu.co>
2024-11-28 16:29:14 -0500

    cambios para el cors

--- src/main/java/BookSwap/security/SecurityConfig.java
+++ src/main/java/BookSwap/security/SecurityConfig.java
@@ -36,7 +36,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173"));
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173", "https://bookswap-app-782468de7a43.herokuapp.com"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(true);
